541896df68801beb6b3931894d17159387e1fcac
Chris Smith <csmith91286@gmail.com>
2015-11-08 13:47:41 -0800

    initialCommit
    Initial Commit to GitHub and testing variables

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    $pageTitle = "Foodshow 2015";
+    $pageHeader = "Goldstar Norcal Foodshow 2015";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo $pageTitle; ?></title>
+</head>
+<body>
+
+</body>
+
+
+
+</html>
---
+++ readme.md
@@ -0,0 +1,6 @@
+#Food show 2016
+Food show registration page for both vendors and customers using PHP and using PHP Storm.
+
+##Change Log
+**11/8/2015**
+Initial Commit
