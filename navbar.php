34cf74f803bbb543ead1e0f68a3570f0665a8257
Chris Smith <csmith91286@gmail.com>
2015-11-12 01:31:46 -0800

    jquery

--- navbar.php
+++ navbar.php
@@ -7,7 +7,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script>
         $(document).ready(function(){
-            $("input['type=text']").on("click", function () {
+            $("input[type='text']").on("click", function () {
                 $(this).select();
             });
         });
