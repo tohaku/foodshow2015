5678509659ef30d893286cfd61c085d909f8a0ea
Chris Smith <csmith91286@gmail.com>
2015-11-12 11:41:13 -0800

    PHP Save form data
    On form input error data isn't reset

--- schoolregistration.php
+++ schoolregistration.php
@@ -9,11 +9,11 @@
 <?php require("navbar.php");?>
 
 <?php
-    $schoolName = "";
-    $FName = "";
-    $LName = "";
-    $phoneNumber = "";
-    $email = "";
+    $schoolName = "Enter school name";
+    $FName = "First name";
+    $LName = "Last name";
+    $phoneNumber = "707-111-2222";
+    $email = "email@hostname.com";
     //errors for required fields or validation
     $formError = false;
     $schoolNameError = "";
@@ -60,7 +60,14 @@
             $formError = true;
         }
 
+        //need to add phone number verification function
         $phoneNumber = testInput($_POST["phoneNumber"]);
+
+        //post the information if there's no problems
+        if(!$formError){
+            //test echo
+            echo $schoolName . "<br>" . $FName . "<br>" . $LName . "<br>" . $phoneNumber . "<br>" . $email;
+        }
     }
 
     //SQL injection prevention, normalize data
@@ -76,22 +83,18 @@
     <form method="post" action="<?php $_PHP_SELF ?>" class="registrationForms" name="schoolRegistration">
         <?php if($formError){echo "<p class='formError'>Error: Please recheck your information</p>";}?>
         <label for="schoolName">School Name:</label>
-        <input type="text" value="Enter school name" id="schoolName" class="<?php if($schoolNameError=="*"){echo "formError";}?>" name="schoolName"/><span class="formError"><?php echo $schoolNameError;?></span><br>
+        <input type="text" value="<?php echo $schoolName;?>" id="schoolName" class="<?php if($schoolNameError=="*"){echo "formError";}?>" name="schoolName"/><span class="formError"><?php echo $schoolNameError;?></span><br>
         <label for="FName">First Name:</label>
-        <input type="text" value="First name" id="FName" class="<?php if($FNameError=="*"){echo "formError";}?>" name="FName"/><span class="formError"><?php echo $FNameError;?></span><br>
+        <input type="text" value="<?php echo $FName;?>" id="FName" class="<?php if($FNameError=="*"){echo "formError";}?>" name="FName"/><span class="formError"><?php echo $FNameError;?></span><br>
         <label for="LName">Last Name:</label>
-        <input type="text" value="Last name" id="LName" class="<?php if($LNameError=="*"){echo "formError";}?>" name="LName"/><span class="formError"><?php echo $LNameError;?></span><br>
+        <input type="text" value="<?php echo $LName;?>" id="LName" class="<?php if($LNameError=="*"){echo "formError";}?>" name="LName"/><span class="formError"><?php echo $LNameError;?></span><br>
         <label for="phoneNumber">Phone Number:</label>
-        <input type="text" value="707-111-2222" id="phoneNumber" name="phoneNumber"/><br>
+        <input type="text" value="<?php echo $phoneNumber;?>" id="phoneNumber" name="phoneNumber"/><br>
         <label for="email">Email:</label>
-        <input type="text" value="email@hostname.com" id="email" class="<?php if($emailError=="*"){echo "formError";}?>" name="email"/><span class="formError"><?php echo $emailError;?></span><br>
+        <input type="text" value="<?php echo $email;?>" id="email" class="<?php if($emailError=="*"){echo "formError";}?>" name="email"/><span class="formError"><?php echo $emailError;?></span><br>
         <input type="submit" value="Register"/>
     </form>
 </div>
 
-<?php
-    //testing that php values were posted and received by webpage
-    echo $schoolName . "<br>" . $FName . "<br>" . $LName . "<br>" . $phoneNumber . "<br>" . $email;
-?>
 </body>
 </html>
