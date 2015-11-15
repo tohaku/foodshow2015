5651e0db34961b693236bd6e657509ad66835acd
Chris Smith <csmith91286@gmail.com>
2015-11-15 13:14:23 -0800

    test

--- boothmap.php
+++ boothmap.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <head>
     <script>
-        function registerBooth(){
+        function registerBooth(test){
             alert("A booth has been clicked");
-            alert($(this).attr("id"));
+            alert(test);
         }
     </script>
 </head>
@@ -22,25 +22,25 @@ $counter = 1;
 while ($counter<=$totalBooths){
     if($counter<18) {
         if($counter == 1){echo "<div id='boothRow1'>";}
-        echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth()'>" . $counter . "</div>";
+        echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth(this.id)'>" . $counter . "</div>";
     }
     elseif($counter<35){
         if($counter == 18) {
             echo "</div><div id='boothRow2'>";
         }
-        echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth()'>" . $counter . "</div>";
+        echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth(this.id)'>" . $counter . "</div>";
     }
     elseif($counter<52){
         if($counter == 35){
             echo "</div><div id='boothRow3'>";
         }
-        echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth()'>" . $counter . "</div>";
+        echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth(this.id)'>" . $counter . "</div>";
     }
     else {
         if($counter == 52){
             echo "</div><div id='boothRow4'>";
         }
-        echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth()'>" . $counter . "</div>";
+        echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth(this.id)'>" . $counter . "</div>";
         if($counter == $totalBooths){
             echo "</div>";
         }
@@ -49,5 +49,6 @@ while ($counter<=$totalBooths){
 }
 ?>
     <div class="floormapText"><p>Entrance</p></div>
+    <p>Select the booths you would like to register. Booths that are red have already been registered and can't be selected.</p>
 </div>
 </body>
