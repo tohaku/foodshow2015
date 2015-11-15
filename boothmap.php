df2b4220ca8ce4e8df392117b20bea74f6e2473d
tohaku <csmith91286@gmail.com>
2015-11-15 02:46:46 -0800

    shifting booths over fixing spacing

--- boothmap.php
+++ boothmap.php
@@ -14,24 +14,24 @@
 <?php
 $counter = 1;
 while ($counter<=$totalBooths){
-    if($counter<17) {
+    if($counter<18) {
         if($counter == 1){echo "<div id='boothRow1'>";}
         echo "<div class='booth' id='booth" . $counter . "'>" . $counter . "</div>";
     }
-    elseif($counter<34){
-        if($counter == 17) {
+    elseif($counter<35){
+        if($counter == 18) {
             echo "</div><div id='boothRow2'>";
         }
         echo "<div class='booth' id='booth" . $counter . "'>" . $counter . "</div>";
     }
-    elseif($counter<51){
-        if($counter == 34){
+    elseif($counter<52){
+        if($counter == 35){
             echo "</div><div id='boothRow3'>";
         }
         echo "<div class='booth' id='booth" . $counter . "'>" . $counter . "</div>";
     }
     else {
-        if($counter == 51){
+        if($counter == 52){
             echo "</div><div id='boothRow4'>";
         }
         echo "<div class='booth' id='booth" . $counter . "'>" . $counter . "</div>";
