118681d6ff6eb5062b1209e26d1feaf0d3ee1a45
Chris Smith <csmith91286@gmail.com>
2015-11-15 13:17:26 -0800

    added map to vendor registration

--- vendorregistration.php
+++ vendorregistration.php
@@ -27,6 +27,55 @@
         <div id="boothNumbers">Append booth numbers here, ie 1,22,33</div>
     </form>
 </div>
+<?php
+$totalBooths = 69;
+?>
+
+<?php require("navbar.php");?>
+
+<div id="floorMap">
+    <div class="floormapText"><p>Kitchen and Restrooms</p></div>
+    <?php
+    $counter = 1;
+    while ($counter<=$totalBooths){
+        if($counter<18) {
+            if($counter == 1){echo "<div id='boothRow1'>";}
+            echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth(this.id)'>" . $counter . "</div>";
+        }
+        elseif($counter<35){
+            if($counter == 18) {
+                echo "</div><div id='boothRow2'>";
+            }
+            echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth(this.id)'>" . $counter . "</div>";
+        }
+        elseif($counter<52){
+            if($counter == 35){
+                echo "</div><div id='boothRow3'>";
+            }
+            echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth(this.id)'>" . $counter . "</div>";
+        }
+        else {
+            if($counter == 52){
+                echo "</div><div id='boothRow4'>";
+            }
+            echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth(this.id)'>" . $counter . "</div>";
+            if($counter == $totalBooths){
+                echo "</div>";
+            }
+        }
+        $counter++;
+    }
+    ?>
+    <div class="floormapText"><p>Entrance</p></div>
+    <p>Select the booths you would like to register. Booths that are red have already been registered and can't be selected.</p>
+</div>
+
+<script>
+    function registerBooth(test){
+        alert("A booth has been clicked");
+        alert(test);
+    }
+</script>
 
 </body>
 </html>
