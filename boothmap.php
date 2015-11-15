965d9461d31e8d5a4f3b72570adc8f268e7fc0f5
tohaku <csmith91286@gmail.com>
2015-11-15 02:29:01 -0800

    hopeful booth margin changes to css

--- boothmap.php
+++ boothmap.php
@@ -16,25 +16,25 @@ $counter = 1;
 while ($counter<=$totalBooths){
     if($counter<17) {
         if($counter == 1){echo "<div id='boothRow1'>";}
-        echo "<div class='booth' id='" . $counter . "'>" . $counter . "</div>";
+        echo "<div class='booth' id='booth" . $counter . "'>" . $counter . "</div>";
     }
     elseif($counter<34){
         if($counter == 17) {
             echo "</div><div id='boothRow2'>";
         }
-        echo "<div class='booth' id='" . $counter . "'>" . $counter . "</div>";
+        echo "<div class='booth' id='booth" . $counter . "'>" . $counter . "</div>";
     }
     elseif($counter<51){
         if($counter == 34){
             echo "</div><div id='boothRow3'>";
         }
-        echo "<div class='booth' id='" . $counter . "'>" . $counter . "</div>";
+        echo "<div class='booth' id='booth" . $counter . "'>" . $counter . "</div>";
     }
     else {
         if($counter == 51){
             echo "</div><div id='boothRow4'>";
         }
-        echo "<div class='booth' id='" . $counter . "'>" . $counter . "</div>";
+        echo "<div class='booth' id='booth" . $counter . "'>" . $counter . "</div>";
         if($counter == $totalBooths){
             echo "</div>";
         }
