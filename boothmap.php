23ecbcd70f89d338503b69e7dead33881d67a277
Chris Smith <csmith91286@gmail.com>
2015-11-18 11:46:13 -0800

    testing

--- boothmap.php
+++ boothmap.php
@@ -1,31 +1,35 @@
 <?php require("navbar.php");?>
 
 <?php
-$totalBooths = 69;
+    $totalBooths = 69;
+    //loading DB values into array on page load
+    //saves amount of DB connections with not being called by function
+    $galifrey = [];//array to store all DB results
+    $dbconn = mysql_connect($dbhost, $dbuser, $dbpass);
+    if (!dbconn) {
+        die("Couldn't connect to the database" . mysql_error());
+    }
+    $sql = "SELECT booths FROM registeredBooths";
+    mysql_select_db('foodshow2015');
+    $retval = mysql_query($sql, $dbconn);
+
+    while($row = mysql_fetch_array($retval, MYSQL_ASSOC)){
+        $tempArray=explode(",",$row["booths"]);
+        print_r($tempArray);
+
+    }
+    mysql_close($dbconn);
+
     function reservedBoothCheck($boothNumber)
     {
-        $dbconn = mysql_connect($dbhost, $dbuser, $dbpass);
-        if (!dbconn) {
-            die("Couldn't connect to the database" . mysql_error());
-        }
-        $sql = "SELECT booths FROM registeredBooths";
-        mysql_select_db('foodshow2015');
-        $retval = mysql_query($sql, $dbconn);
 
-        while($row = mysql_fetch_array($retval, MYSQL_ASSOC)){
-            echo $row["booths"];
-            if($row["booths"]===$boothNumber){
-                mysql_close($dbconn);
-                return true;
-            }
-        }
         /*i don't need to put into JS at all, I can just use PHP with the existing PHP that creates the map.
         *create a php function that can be called in the create map below, if it returns false give it a different
         *ID that can be styled differently.
          * Then on the JS that add the booth to the array / form field, do a check on the id and if it's a no no
          * don't add.
         */
-        mysql_close($dbconn);
+
         return false;
     }
 ?>
