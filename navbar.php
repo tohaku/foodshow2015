1679d38464795515f3d0deded31ab9e0e64fc411
Jonathan Hansen <toadridesbikes@gmail.com>
2015-11-25 11:10:33 -0800

    Added Charset to <head>

--- navbar.php
+++ navbar.php
@@ -3,6 +3,7 @@
 <html lang="en">
 <head>
     <title><?php echo $pageTitle; ?></title>
+    <meta charset="utf-8" />
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css">
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
