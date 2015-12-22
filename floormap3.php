20cbfefd2ea184e794158f732f17351112f682f2
Chris Smith <csmith91286@gmail.com>
2015-12-22 12:29:49 -0800

    floormap3 creation

--- floormap3.php
+++ floormap3.php
@@ -1,68 +1,7 @@
 <?php require("navbar.php");?>
-<!--
-<head>
-<style>
-    #floorMap {
-        width: 820px; /*added one more booth width*/
-        min-height: 455px; /*added addition row height*/
-        margin: 40px auto 10px;
-        padding: 2px;
-        background-color: #fff;
-        border: inset 2px #000;
-    }
-    #boothRow1{
-        margin-top: 35px;
-        margin-left: 35px;
-    }
 
-    #boothRow2 {
-        margin-left: 35px;
-    }
-
-    #boothRow3 {
-        margin-top: 35px;
-        margin-left: 35px;
-    }
-
-    #boothRow4 {
-        margin-left: 35px;
-    }
-    #boothRow5 {
-        margin-top: 35px;
-    }
-
-    .booth {
-        display: inline-block;
-        width: 35px;
-        height: 35px;
-        border: 1px solid #000;
-        text-align: center;
-        font-size: 18px;
-        padding: 2px;
-    }
-
-    .boothReserved {
-        display: inline-block;
-        width: 35px;
-        height: 35px;
-        border: 1px solid #000;
-        text-align: center;
-        font-size: 18px;
-        padding: 2px;
-        background-color: #F00000;
-    }
-    #booth40, #booth56, #booth42, #booth58 {
-        margin-left: 35px;
-    }
-    #booth9, #booth25, #booth74 {
-        margin-left: 70px;
-    }
-</style>
-    </head>
-<body>
--->
 <?php
-$totalBooths = 82;
+$totalBooths = 93;
 $galifrey = [];
 //loading DB values into array on page load
 //saves amount of DB connections with not being called by function
@@ -92,7 +31,7 @@ $conn = null;
     $counter = 1;
     while ($counter<=$totalBooths){
         $timeLords = "booth".$counter;
-        if($counter<17) {
+        if($counter<18) {
             if($counter == 1){echo "<div id='boothRow1'>";}
             if(in_array($timeLords,$galifrey2)){
                 echo "<div class='boothReserved' id='booth" . $counter . "'>X</div>";
@@ -101,28 +40,35 @@ $conn = null;
                 echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth(this.id)'>" . $counter . "</div>";
             }
         }
-        elseif($counter<33){
-            if($counter == 17) {
+        elseif($counter<35){
+            if($counter == 18) {
                 echo "</div><div id='boothRow2'>";
             }
             echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth(this.id)'>" . $counter . "</div>";
         }
-        elseif($counter<49){
-            if($counter == 33){
+        elseif($counter<52){
+            if($counter == 18){
                 echo "</div><div id='boothRow3'>";
             }
             echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth(this.id)'>" . $counter . "</div>";
         }
-        elseif($counter<65){
-            if($counter == 49){
+        elseif($counter<69){
+            if($counter == 52){
                 echo "</div><div id='boothRow4'>";
             }
             echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth(this.id)'>" . $counter . "</div>";
         }
-        else {
-            if($counter == 65){
+        elseif($counter<88){
+            if($counter == 69){
                 echo "</div><div id='boothRow5'>";
             }
+            echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth(this.id)'>" . $counter . "</div>";
+        }
+        elseif($counter == 88){
+            echo "</div><div id='boothRow6'>";
+            echo "<div class='booth' id='booth88'>Registration</div>";
+        }
+        else{
             echo "<div class='booth' id='booth" . $counter . "' onclick='registerBooth(this.id)'>" . $counter . "</div>";
             if($counter == $totalBooths){
                 echo "</div>";
